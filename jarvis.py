32912e81bdfa51f8b52b4ba8f5fb60902382d503
Emedp <migueldinizp@gmail.com>
2023-07-16 02:56:59 +0200

    INITIAL COMMIT

---
+++ jarvis.py
@@ -0,0 +1,32 @@
+'''
+JARVIS CORE
+'''
+# TODO loop
+# TODO GUI
+
+'''
+TRANSCRIBIR VOZ - TEXTO
+'''
+# TODO speech to text
+# TODO text to speech
+# TODO escuchar con el micro
+
+'''
+DATOS DE TIEMPO Y LUGAR
+'''
+# TODO datos de día y hora
+# TODO datos de clima
+
+'''
+MONITORIZACIÓN Y CONTROL DEL EQUIPO
+'''
+# TODO datos del sistema como batería, RAM, ROM, CPU, GPU, etc
+# TODO cambiar valores del sistema como volumen, etc
+# TODO check internet velocity
+
+'''
+BUSCAR INFORMACIÓN
+'''
+# TODO buscar en google
+# TODO buscar en wikipedia
+# TODO hablar con ChatGPT
