a15192c9ad7cc4ca001df663594a294be535d9ab
Emedp <migueldinizp@gmail.com>
2023-07-17 18:07:27 +0200

    speech to text

--- jarvis.py
+++ jarvis.py
@@ -1,3 +1,5 @@
+import speech_recognition #https://pypi.org/project/SpeechRecognition/
+
 '''
 JARVIS CORE
 '''
@@ -7,7 +9,31 @@ JARVIS CORE
 '''
 TRANSCRIBIR VOZ - TEXTO
 '''
-# TODO speech to text
+# speech recognition
+def speech_to_text ():
+    audio = None
+    stt = ""
+    stt_google = None
+
+    # obtain audio from microphone
+    recognizer = speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source, duration=0.5) # listen for 0.5 second, the minimum, to calibrate the energy threshold for ambient noise levels
+        print("Say something...")
+        audio = recognizer.listen(source)
+
+    # try recognize speech using Google Speech Recognition
+    try:
+        stt_google = recognizer.recognize_google(audio, language="es-ES")
+        print("Google Speech Recognition thinks you said: " + stt_google)
+    except speech_recognition.UnknownValueError:
+        print("Google Speech Recognition could not understand audio. Try again")
+    except speech_recognition.RequestError as e:
+        print(f"Google Speech Recognition could not be available now. Request failed. Error: {e}")
+
+    stt = str(stt_google)
+    return stt
+
 # TODO text to speech
 # TODO escuchar con el micro
 
@@ -30,3 +56,5 @@ BUSCAR INFORMACIÓN
 # TODO buscar en google
 # TODO buscar en wikipedia
 # TODO hablar con ChatGPT
+
+speech = speech_to_text()
